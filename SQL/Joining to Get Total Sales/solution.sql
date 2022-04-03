7bc999c7d065b53285b29f105914f9423a3ba59a
Yoiq S Rambadian <yoiqrambadian@gmail.com>
2022-04-03 13:34:26 +0700

    add sample records

--- SQL/Joining to Get Total Sales/solution.sql
+++ SQL/Joining to Get Total Sales/solution.sql
@@ -24,6 +24,24 @@ create table revenue (
     , foreign key (order_id) references orders (order_id)
 );
 
+
+-- populate table orders with sample records
+insert into orders (order_id, channel, date, month) values (1, 'Online', '2018-09-01', 'September');
+insert into orders (order_id, channel, date, month) values (2, 'Online', '2018-09-03', 'September');
+insert into orders (order_id, channel, date, month) values (3, 'In_store', '2018-10-11', 'October');
+insert into orders (order_id, channel, date, month) values (4, 'In_store', '2018-08-21', 'August');
+insert into orders (order_id, channel, date, month) values (5, 'Online', '2018-08-13', 'August');
+insert into orders (order_id, channel, date, month) values (6, 'Online', '2018-10-29', 'October');
+
+-- populate table orders with sample records
+insert into revenue (order_id, revenue) values (1, 100);
+insert into revenue (order_id, revenue) values (2, 125);
+insert into revenue (order_id, revenue) values (3, 200);
+insert into revenue (order_id, revenue) values (4, 80);
+insert into revenue (order_id, revenue) values (5, 200);
+insert into revenue (order_id, revenue) values (6, 100);
+
+
 -- Solution
 select
     o.channel,
